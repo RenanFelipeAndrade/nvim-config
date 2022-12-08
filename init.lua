4c3bb8d9bac53af4e4acbf12458bd79069b006d0
Renan Andrade <fel9.renan02@gmail.com>
2022-12-08 16:42:44 -0300

    Intialized repository

---
+++ init.lua
@@ -0,0 +1,104 @@
+-- variables
+local Plug = vim.fn["plug#"]
+
+-- begin plugins
+vim.call("plug#begin")
+
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-tree/nvim-tree.lua'
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
+vim.call("plug#end")
+
+require('lspconfig').pyright.setup({})
+require("nvim-tree").setup()
+
+-- neovim tree config
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- keymaps
+vim.g.mapleader = "<space>"
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+
+vim.keymap.set('n', '<space>e', '<cmd>NvimTreeToggle<cr>')
+vim.keymap.set('n', '<space>c', '<cmd>q<cr>')
+vim.keymap.set('n', '<space>w', '<cmd>w<cr>')
+vim.keymap.set('n', '<space>q', '<cmd>wq<cr>')
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+snippet = {
+-- REQUIRED - you must specify a snippet engine
+expand = function(args)
+-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+end,
+},
+window = {
+-- completion = cmp.config.window.bordered(),
+-- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+['<C-b>'] = cmp.mapping.scroll_docs(-4),
+['<C-f>'] = cmp.mapping.scroll_docs(4),
+['<C-Space>'] = cmp.mapping.complete(),
+['<C-e>'] = cmp.mapping.abort(),
+['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+{ name = 'nvim_lsp' },
+-- { name = 'vsnip' }, -- For vsnip users.
+-- { name = 'luasnip' }, -- For luasnip users.
+-- { name = 'ultisnips' }, -- For ultisnips users.
+-- { name = 'snippy' }, -- For snippy users.
+}, {
+{ name = 'buffer' },
+})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+sources = cmp.config.sources({
+{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+}, {
+{ name = 'buffer' },
+})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+mapping = cmp.mapping.preset.cmdline(),
+sources = {
+{ name = 'buffer' }
+}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+mapping = cmp.mapping.preset.cmdline(),
+sources = cmp.config.sources({
+{ name = 'path' }
+}, {
+{ name = 'cmdline' }
+})
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['pyright'].setup {
+capabilities = capabilities
+}
