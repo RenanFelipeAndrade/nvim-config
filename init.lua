90c445864e094f20167c6cfb5a2eaffcb656e54e
Renan Andrade <fel9.renan02@gmail.com>
2023-02-01 15:22:02 -0300

    Installed plugins, made simple setup

--- init.lua
+++ init.lua
@@ -1,104 +1,93 @@
+-- nvim config
+vim.o.relativenumber = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- variables
 local Plug = vim.fn["plug#"]
 
 -- begin plugins
 vim.call("plug#begin")
 
-Plug 'nvim-tree/nvim-web-devicons'
-Plug 'nvim-tree/nvim-tree.lua'
 Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
+Plug 'glepnir/lspsaga.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-tree/nvim-tree.lua'
+Plug 'airblade/vim-rooter'
 
 vim.call("plug#end")
 
-require('lspconfig').pyright.setup({})
-require("nvim-tree").setup()
-
--- neovim tree config
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim options
+local option = vim.o
+option.noswapfile = true
+option.tabstop = 4
+option.shiftwidth = 4
+option.expandtab = true
 
 -- keymaps
-vim.g.mapleader = "<space>"
+vim.g.mapleader = " "
 
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+local keyset = vim.keymap.set
+keyset('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+keyset('n', '<leader>c', '<cmd>q<cr>')
+keyset('n', '<leader>w', '<cmd>w<cr>')
+keyset('n', '<leader>q', '<cmd>wq<cr>')
 
-vim.keymap.set('n', '<space>e', '<cmd>NvimTreeToggle<cr>')
-vim.keymap.set('n', '<space>c', '<cmd>q<cr>')
-vim.keymap.set('n', '<space>w', '<cmd>w<cr>')
-vim.keymap.set('n', '<space>q', '<cmd>wq<cr>')
+-- neovim tree config
+require("nvim-tree").setup({
+    view = {
+    relativenumber = true
+    },
+    sync_root_with_cwd = true,
+    -- respect_buf_cwd = true,
+    -- update_focused_file = {
+    --     enable = true,
+    --     update_root = true
+    -- }
+})
 
--- Set up nvim-cmp.
-local cmp = require'cmp'
-
+-- cmp config
+local cmp = require("cmp")
 cmp.setup({
-snippet = {
--- REQUIRED - you must specify a snippet engine
-expand = function(args)
--- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
--- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
--- require('snippy').expand_snippet(args.body) -- For `snippy` users.
--- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-end,
-},
-window = {
--- completion = cmp.config.window.bordered(),
--- documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
-['<C-b>'] = cmp.mapping.scroll_docs(-4),
-['<C-f>'] = cmp.mapping.scroll_docs(4),
-['<C-Space>'] = cmp.mapping.complete(),
-['<C-e>'] = cmp.mapping.abort(),
-['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-}),
-sources = cmp.config.sources({
-{ name = 'nvim_lsp' },
--- { name = 'vsnip' }, -- For vsnip users.
--- { name = 'luasnip' }, -- For luasnip users.
--- { name = 'ultisnips' }, -- For ultisnips users.
--- { name = 'snippy' }, -- For snippy users.
-}, {
-{ name = 'buffer' },
-})
-})
+    mapping = cmp.mapping.preset.insert({
+      ['<C-j>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-k>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
-{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-}, {
-{ name = 'buffer' },
-})
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-mapping = cmp.mapping.preset.cmdline(),
-sources = {
-{ name = 'buffer' }
-}
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-mapping = cmp.mapping.preset.cmdline(),
-sources = cmp.config.sources({
-{ name = 'path' }
-}, {
-{ name = 'cmdline' }
-})
-})
-
--- Set up lspconfig.
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+    },{
+      { name = 'buffer' },
+      }
+    )})
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['pyright'].setup {
+require('lspconfig')['tsserver'].setup {
 capabilities = capabilities
 }
+
+-- mason config
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+-- mason-lspconfig config
+require("mason-lspconfig").setup({
+    automatic_installation = true
+})
